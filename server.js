8cb381b853dbafa29a5c3ea6fcd48514ee1a5caf
raool <rs4414420@icloud.com>
2025-07-16 12:43:51 +0530

    updated

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
